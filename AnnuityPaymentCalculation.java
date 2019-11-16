df33949452aa424ca08362dc6ae737da24518657
dokstorm <gado97@mail.ru>
2019-11-16 19:25:18 +0400

    Delete AnnuityPaymentCalculation.java

--- AnnuityPaymentCalculation.java
+++
@@ -1,8 +0,0 @@
-import java.math.BigDecimal;
-
-public class AnnuityPaymentCalculation implements PaymentCalculation {
-    @Override
-    public BigDecimal calculatePayment(int currentMonth, CreditDetail creditDetail) {
-        return null;
-    }
-}
