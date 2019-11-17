8168b381853d2eca0dc4df9700c3cacf5e7b4e5b
dokstorm <gado97@mail.ru>
2019-11-17 22:49:51 +0400

    Delete InterestRateCalculation.java

--- paymentcalculation/InterestRateCalculation.java
+++
@@ -1,9 +0,0 @@
-package paymentcalculation;
-
-import paymentcalculation.PaymentType;
-
-import java.math.BigDecimal;
-
-public interface InterestRateCalculation {
-    double calculateInterestRate(BigDecimal amount, int term, PaymentType paymentType);
-}
