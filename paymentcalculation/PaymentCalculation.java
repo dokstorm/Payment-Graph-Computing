6dc7ad18cb5b3102d2989516fd45957da31f432e
dokstorm <gado97@mail.ru>
2019-11-17 22:50:12 +0400

    Delete PaymentCalculation.java

--- paymentcalculation/PaymentCalculation.java
+++
@@ -1,8 +0,0 @@
-package paymentcalculation;
-
-import java.math.BigDecimal;
-import java.util.List;
-
-public interface PaymentCalculation {
-    List<Payment> calculatePayment(CreditDetail creditDetail);
-}
